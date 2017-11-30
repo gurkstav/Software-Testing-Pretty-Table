040264d03195bc2943b2165aa6a9d169aa720b27
ClovsBob <clovis.bobillier@hotmail.fr>
2017-11-30 12:46:07 +0100

    update w/ validate_attributes

--- tests_clovis.py
+++ tests_clovis.py
@@ -11,7 +11,7 @@ class TestPrettyTable(unittest.TestCase):
 		self.pt._field_names = ["City name", "Area", "Population", "Annual Rainfall"]
 		self.pt.add_row(["Adelaide",1295, 1158259, 600.5])
 		
-		#return self
+		
 
 
 		
@@ -27,7 +27,7 @@ class TestPrettyTable(unittest.TestCase):
 		self.assertEqual(table._rows[0],["Adelaide",1295, 1158259, 600.5,"Joh"])
 		self.assertEqual(table._field_names,["City name", "Area", "Population", "Annual Rainfall","Mayor"])
 
-		#table.add_row(["Brisbane", 5905, 1857594, 1146.4])
+		
 
 	def test_clear_rows(self):
 
@@ -51,14 +51,29 @@ class TestPrettyTable(unittest.TestCase):
 		self.assertTrue(not table._field_names)#true if list is empty
 		self.assertTrue(not table._widths)#true if list is empty
 
-	
+	def test_validate_attributes(self):
+		
+		
+		dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+		try:
+			
+			self.pt._validate_attributes("a variable",dict)
+			self.pt._validate_header_style(None)
+		except Exception:
+			self.fail('_validate_header_style raised an Exception for valid input')
+		
+		self.assertRaises(Exception, self.pt._validate_attributes,"a variable", "aa")
+		self.assertRaises(Exception, self.pt._validate_attributes,"a variable", "")
+		self.assertRaises(Exception, self.pt._validate_attributes,"a variable")
+		self.assertRaises(Exception, self.pt._validate_attributes,"a variable", 0)
+		self.assertRaises(Exception, self.pt._validate_attributes,"a variable", 1)
+		self.assertRaises(Exception, self.pt._validate_attributes,"a variable", 2)
+
+		
 
 
 if __name__ == '__main__':
-	#test=PrettyTable()
-
-	#test=setUp(test)
-	#test_add_column(test)
+	
 	unittest.main()
 
 
